b8ff89cd1ec370cd4ed461791aa7678769777b04
winterdev001 <snowdevin.sd@gmail.com>
2022-07-19 11:29:03 +0200

    settings fix

--- refactoring_sample/settings.py
+++ refactoring_sample/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'refactoring_sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ajax_example',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
